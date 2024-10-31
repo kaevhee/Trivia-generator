8a9c1d9b63c29fd10c0033a94ad08fc8e0103ce8
kaweng pam <kawengpam56@gmail.com>
2024-10-31 21:22:55 +0100

    added basic javascript function and typerwriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateTrivia(event) {
+  event.preventDefault();
 
+  new Typewriter("#trivia-question", {
+    strings: ["Which freezes faster: hot or cold water?"],
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+let triviaGeneratorElement = document.querySelector("#trivia-generator");
+triviaGeneratorElement.addEventListener("submit", generateTrivia);
