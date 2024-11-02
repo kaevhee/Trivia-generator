fd3fc6791ca345efeb12fe821dc74429502b9b4e
kaweng pam <kawengpam56@gmail.com>
2024-11-02 17:14:54 +0100

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 function displayTrivia(response) {
-  console.log("Trivia question generated!");
-
   new Typewriter("#trivia-question", {
     strings: response.data.answer,
     autoStart: true,
@@ -16,12 +14,12 @@ function generateTrivia(event) {
   let context =
     "You are knowledgeable about trivia question concerning any topic. please generate a thought provoking and short trivia question about any subject along with it's answer in <br>  and sign the trivia question with the origin in <strong>. make sure to follow the User guide below.";
   let prompt = `User guide: Generate a Trivia about ${userCommand.value}`;
-
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating trivia...");
-  console.log(prompt)
-  console.log(context)
+  let triviaElement = document.querySelector("#trivia-question");
+  triviaElement.classList.remove("hidden");
+  triviaElement.innerHTML = `<div class="generating">⏳ Generating a trivia question about ${userCommand.value}...<div>`;
+
   axios.get(apiURL).then(displayTrivia);
 }
 let triviaGeneratorElement = document.querySelector("#trivia-generator");
